bcf323a77d5a1f4d608547f736475e0f3d9da2a1
vaglevor <antonyasinski@mail.ru>
2025-10-22 22:40:31 +0300

    Add files via upload

---
+++ task20.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class task20 {
+
+        public static List<Integer> findAllDivisors(int n){
+            ArrayList<Integer> divs = new ArrayList<>();
+            for (int i = 1; i * i <= n; i++) {
+                if (n / i == 0) {
+                    divs.add(i);
+                    if (i * i != 0) {
+                        divs.add(n / i);
+                    }
+                }
+            }
+            return divs;
+        }
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            int sum = 0;
+            for (int i : findAllDivisors(num)) {
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+    }
