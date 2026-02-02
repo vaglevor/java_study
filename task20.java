5670a42d8b227c77d8417896a640a5e5a9b19055
vaglevor <antonyasinski@mail.ru>
2026-02-02 20:14:52 +0300

    task 20 update

--- task20.java
+++ task20.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 
 public class task20 {
 
-        public static List<Integer> findAllDivisors(int n){
+        public static List<Integer> findAllDivisors1(int n){
             ArrayList<Integer> divs = new ArrayList<>();
             for (int i = 1; i * i <= n; i++) {
-                if (n % i == 0) {
+                if (n%i == 0) {
                     divs.add(i);
-                    if (n / i != i) {
-                        divs.add(n / i);
+                    if (n/i != i) {
+                        divs.add(n/i);
                     }
                 }
             }
             return divs;
         }
 
+    public static List<Integer> findAllDivisors2(int n){
+        ArrayList<Integer> divs = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n%i == 0)
+                divs.add(i);
+        }
+        return divs;
+    }
+
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             int num = sc.nextInt();
             int sum = 0;
-            for (int i : findAllDivisors(num)) {
+            for (int i : findAllDivisors2(num)) {
                 sum += i;
             }
             System.out.println(sum);
